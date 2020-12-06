3c819b7f70ba7ef8d60f0ddcb22db8908fc042ad
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2020-12-06 17:46:09 -0300

    Listando as transações

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -10,8 +10,29 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  public async getBalance(): Promise<void> {
-    // TODO
+  public async getBalance(): Promise<Balance> {
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (accumulator, transaction) => {
+        accumulator.income +=
+          transaction.type === 'income' ? Number(transaction.value) : 0;
+
+        accumulator.outcome +=
+          transaction.type === 'outcome' ? Number(transaction.value) : 0;
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
